97ca8fc80d604be456c0912f33b4a45ac1506b56
SeanKang <sk92129@gmail.com>
2024-12-11 14:09:25 -0800

    init

---
+++ src/main/java/kangengineering/App.java
@@ -0,0 +1,18 @@
+package kangengineering;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+
+@RestController
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
